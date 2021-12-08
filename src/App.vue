6b3a2bbe656293aceb7fba5043ce832ba60588e2
Amirani <amiko19991@gmail.com>
2021-12-08 14:49:07 +0400

    push app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="youtube">
+    <input
+      placeholder="find Video"
+      class="youtube__input"
+      type="text"
+      v-model="videoName"
+    />
+    <button class="youtube__button" @click="searchVideo">SEARCH</button>
+    <iframe
+      class="youtube__player"
+      width="560"
+      height="315"
+      :src="youtubeLink"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      api: "https://www.googleapis.com/youtube/v3/search",
+      videoName: "",
+      videoId: "",
+      key: "AIzaSyB8OKTW0EC_taAgJSyi7pg2fyFftPVVum8",
+    };
+  },
+  methods: {
+    async searchVideo() {
+      const url = `${this.api}?key=${this.key}&q=${this.videoName}&part=snippet&maxResults=1`;
+      let data = await fetch(url);
+
+      let videoData = await data.json();
+      this.videoId = videoData.items[0].id.videoId;
+    },
+  },
+  computed: {
+    youtubeLink() {
+      return `https://www.youtube.com/embed/${this.videoId}`;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.youtube {
+  padding-top: 5em;
+  width: 30%;
+  min-width: 550px;
+  margin: auto;
+  display: flex;
+  gap: 2em;
+  flex-direction: column;
+}
+
+.youtube__input {
+  padding: 1em;
+  border-bottom: 1px solid black;
+  outline: none;
+}
+
+.youtube__button {
+  padding: 1em;
+  background-color: brown;
+  cursor: pointer;
+}
+
+.youtube__button:hover {
+  opacity: 0.7;
+}
+
+.youtube__player {
+  width: 100%;
 }
 </style>
