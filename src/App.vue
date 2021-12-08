48b2efbf705f309824821c58513dd11251a33d55
Amirani <amiko19991@gmail.com>
2021-12-08 15:52:45 +0400

    change

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,20 @@
       placeholder="find Video"
       class="youtube__input"
       type="text"
-      v-model="videoName"
+      v-model.lazy="videoName"
     />
-    <button class="youtube__button" @click="searchVideo">SEARCH</button>
-    <iframe
-      class="youtube__player"
-      width="560"
-      height="315"
-      :src="youtubeLink"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+    <div v-for="video in youtubeVideos" :key="video.id">
+      <iframe
+        class="youtube__player"
+        width="560"
+        height="315"
+        :src="youtubeLink(video.id.videoId)"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
   </div>
 </template>
 
@@ -27,22 +28,27 @@ export default {
     return {
       api: "https://www.googleapis.com/youtube/v3/search",
       videoName: "",
-      videoId: "",
-      key: "AIzaSyB8OKTW0EC_taAgJSyi7pg2fyFftPVVum8",
+      youtubeVideos: [],
+      Apikey: "AIzaSyAnnzTDzk1ASrSq7H2XT41p0dxulRk5ZOQ",
     };
   },
   methods: {
-    async searchVideo() {
-      const url = `${this.api}?key=${this.key}&q=${this.videoName}&part=snippet&maxResults=1`;
-      let data = await fetch(url);
+    async searchVideo(videoName) {
+      const url = `${this.api}?key=${this.Apikey}&q=${videoName}&maxResults=5`;
 
+      let data = await fetch(url);
       let videoData = await data.json();
-      this.videoId = videoData.items[0].id.videoId;
+      console.log(videoData);
+      this.youtubeVideos = videoData.items;
+      console.log(this.youtubeVideos);
+    },
+    youtubeLink(id) {
+      return `https://www.youtube.com/embed/${id}`;
     },
   },
-  computed: {
-    youtubeLink() {
-      return `https://www.youtube.com/embed/${this.videoId}`;
+  watch: {
+    videoName: function (value) {
+      this.searchVideo(value);
     },
   },
 };
@@ -58,23 +64,19 @@ export default {
   gap: 2em;
   flex-direction: column;
 }
-
 .youtube__input {
   padding: 1em;
   border-bottom: 1px solid black;
   outline: none;
 }
-
 .youtube__button {
   padding: 1em;
   background-color: brown;
   cursor: pointer;
 }
-
 .youtube__button:hover {
   opacity: 0.7;
 }
-
 .youtube__player {
   width: 100%;
 }
